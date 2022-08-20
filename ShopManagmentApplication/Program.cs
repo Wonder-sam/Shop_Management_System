7a1c2ed75f609c88f0530c5d5e0dd10e6cec9a4f
wonder <samuel.nai@yahoo.com>
2022-08-20 01:58:13 +0000

    initialize login screen as landing screen

---
+++ ShopManagmentApplication/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using ShopManagementApp.database;
+using ShopManagementApp.screens;
+using MySql.Data.MySqlClient;
+using static System.Console;
+namespace ShopManagementApp
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Application.Run(new Login());
+        }
+    }
+}
