830432c20dcee9f9b0939dcde63274d96f245340
wonder <samuel.nai@yahoo.com>
2022-08-22 19:00:38 +0000

    create product category class

---
+++ ShopManagmentApplication/classes/ProductCategory.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShopManagementApplication.classes
+{
+    internal class ProductCategory
+    {
+        public ProductCategory(string category, int numberOfItems)
+        {
+            this.category = category;
+            this.numberOfItems = numberOfItems;
+        }
+        
+        private int cid;
+
+        public int Cid
+        {
+            get { return cid; }
+            set { cid = value; }
+        }
+
+        private string category;
+
+        public string Category
+        {
+            get { return category; }
+            set { category = value; }
+        }
+
+        private int numberOfItems;
+
+        public int NumberOfItems
+        {
+            get { return numberOfItems; }
+            set { numberOfItems = value; }
+        }
+
+
+    }
+}
