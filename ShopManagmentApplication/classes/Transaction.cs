8592e70005db8f0d80a61952edc11071b93d29dc
wonder <samuel.nai@yahoo.com>
2022-08-22 19:01:02 +0000

    create transaction class

---
+++ ShopManagmentApplication/classes/Transaction.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShopManagementApplication.classes
+{
+    internal class Transaction
+    {
+        public Transaction(int pid, DateTime dot, int quantity, float netPrice)
+        {
+            this.pid = pid;
+            this.dot = dot; 
+            this.quantity = quantity;
+            this.netPrice = netPrice;
+        }
+
+        private int tid;
+
+        public int Tid
+        {
+            get { return tid; }
+            set { tid = value; }
+        }
+
+        private int pid;
+
+        public int Pid
+        {
+            get { return pid; }
+            set { pid = value; }
+        }       
+
+        private DateTime dot;
+
+        public DateTime Dot
+        {
+            get { return dot; }
+            set { dot = value; }
+        }
+
+        private int quantity;
+
+        public int Quantity
+        {
+            get { return quantity; }
+            set { quantity = value; }
+        }
+
+        private float netPrice;
+
+        public float NetPrice
+        {
+            get { return netPrice; }
+            set { netPrice = value; }
+        }
+
+    }
+}
