79206ed52203c4a00591bf01b08e494ee7d32692
wonder <samuel.nai@yahoo.com>
2022-08-28 17:59:10 +0000

    add function to retrieve daily sales report from database

--- ShopManagmentApplication/classes/Transaction.cs
+++ ShopManagmentApplication/classes/Transaction.cs
@@ -1,8 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
+using ShopManagementApplication.database;
 
 namespace ShopManagementApplication.classes
 {
@@ -54,6 +57,68 @@ namespace ShopManagementApplication.classes
         {
             get { return netPrice; }
             set { netPrice = value; }
+        }
+
+        public static void GetDailyReport(TableLayoutPanel reportTable)
+        {
+            DatabaseConnection connection = new();
+            string reportQuery = $"select products.productName, products.productCategory, products.productPrice, COUNT(sales.quantity) as Quantity, " +
+                $"SUM(productPrice) as 'Sub Total' from products  INNER JOIN sales ON products.pid = sales.pid WHERE CAST(sales.dos AS DATE) = CURDATE() " +
+                $"group by (products.productName)";
+
+            try
+            {
+                MySqlCommand cmd = new(reportQuery, connection.conn);
+
+                MySqlDataReader reader = cmd.ExecuteReader();
+                DataTable dataTable = new();
+                dataTable.Load(reader);
+                reportTable.RowCount = 1;
+                reportTable.RowStyles[reportTable.RowCount - 1].Height = 40;
+                foreach (DataRow row in dataTable.Rows)
+                {
+                    reportTable.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+                    reportTable.RowCount += 1;
+                    Label label0 = new()
+                    {
+                        Text = row["productName"].ToString()!,
+                    };
+
+                    Label label1 = new()
+                    {
+                        Text = row["productCategory"].ToString()!,
+                    };
+
+                    Label label2 = new()
+                    {
+                        Text = row["productPrice"].ToString()!,
+                    };
+
+                    Label label3 = new()
+                    {
+                        Text = row["Quantity"].ToString()!,
+                    };
+
+                    Label label4 = new()
+                    {
+                        Text = row["Sub Total"].ToString()!,
+                    };
+
+
+
+                    reportTable.Controls.Add(label0, 0, reportTable.RowCount - 1);
+                    reportTable.Controls.Add(label1, 1, reportTable.RowCount - 1);
+                    reportTable.Controls.Add(label2, 2, reportTable.RowCount - 1);
+                    reportTable.Controls.Add(label3, 3, reportTable.RowCount - 1);
+                    reportTable.Controls.Add(label4, 4, reportTable.RowCount - 1);
+                }
+                reader.Close();
+            }
+            catch (Exception err)
+            {
+                MessageBox.Show(err.Message);
+            }
+            connection.conn.Close();
         }
 
     }
