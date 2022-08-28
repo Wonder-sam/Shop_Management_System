6afb3109b9e6c469cbdae13bd2b04a18b8c29149
wonder <samuel.nai@yahoo.com>
2022-08-28 20:51:31 +0000

    add and update dependencies

--- ShopManagmentApplication/Properties/Resources.Designer.cs
+++ ShopManagmentApplication/Properties/Resources.Designer.cs
@@ -63,6 +63,16 @@ namespace ShopManagementApplication.Properties {
         /// <summary>
         ///   Looks up a localized resource of type System.Drawing.Bitmap.
         /// </summary>
+        internal static System.Drawing.Bitmap Group_103 {
+            get {
+                object obj = ResourceManager.GetObject("Group 103", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
         internal static System.Drawing.Bitmap userPhoto {
             get {
                 object obj = ResourceManager.GetObject("userPhoto", resourceCulture);
